3c1390b37a59951b5d31653b466e4eb26c456473
Sherry <hight7501@gmail.com>
2023-09-11 15:47:42 +0800

    佩璇新增產品頁面~

--- index.js
+++ index.js
@@ -1,105 +1,110 @@
 var express = require("express");
 var app = express();
 
-var bp = require('body-parser');
+var bp = require("body-parser");
 var jp = bp.json();
-var cp = require('cookie-parser');
+var cp = require("cookie-parser");
 app.use(cp());
-var {login_render, login_api } = require('./middleware/auth_login');
+var { login_render, login_api } = require("./middleware/auth_login");
 
-
-app.set('view engine', 'ejs');
-require('dotenv').config();
-
-
+app.set("view engine", "ejs");
+require("dotenv").config();
 
 // 首頁
-app.use('/', express.static('lib'));
+app.use("/", express.static("lib"));
 app.get("/", function (req, res) {
-    res.render('home');
-})
+  res.render("home");
+});
 
 // 登入
-app.use('/login', express.static('lib'));
-var login = require('./router/login');
-app.use('/login', login);
+app.use("/login", express.static("lib"));
+var login = require("./router/login");
+app.use("/login", login);
 // app.get('/login', function(req, res){
 //     res.send('這是login數據');
 // })
 
 // 關於我們
-app.use('/about', express.static('lib'));
-var about = require('./router/about');
-app.use('/about', about);
-app.get('/about', function(req, res){
-    res.render('about');
-})
+app.use("/about", express.static("lib"));
+var about = require("./router/about");
+app.use("/about", about);
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
 // 診斷
-app.use('/eval', express.static('lib'));
-var evalu = require('./router/evaluation');
-app.use('/eval', evalu);
-app.get('/eval', function(req, res){
-    res.render('evaluation');
-})
-
+app.use("/eval", express.static("lib"));
+var evalu = require("./router/evaluation");
+app.use("/eval", evalu);
+app.get("/eval", function (req, res) {
+  res.render("evaluation");
+});
 
 // 聯絡我們
-app.use('/contact', express.static('lib'));
-var contact = require('./router/contact');
-app.use('/contact', contact);
-app.get('/contact', function(req, res){
-    res.render('contact');
-})
+app.use("/contact", express.static("lib"));
+var contact = require("./router/contact");
+app.use("/contact", contact);
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
 
 // 常見Q&A
-app.use('/qna', express.static('lib'));
-var qna = require('./router/qna');
-app.use('/qna', qna);
-app.get('/qna', function(req, res){
-    res.render('qna');
-})
+app.use("/qna", express.static("lib"));
+var qna = require("./router/qna");
+app.use("/qna", qna);
+app.get("/qna", function (req, res) {
+  res.render("qna");
+});
 
 // 購物車
-var cart_order = require('./router/cart_order');
+var cart_order = require("./router/cart_order");
 const bodyParser = require("body-parser");
-app.use('/', cart_order);
-app.use('/cart', express.static('lib'));
+app.use("/", cart_order);
+app.use("/cart", express.static("lib"));
 app.get("/cart", login_render, function (req, res) {
-    // console.log('middleware通過成功到購物車');
-    res.render('cart');
-})
+  // console.log('middleware通過成功到購物車');
+  res.render("cart");
+});
+
+// 產品資訊
+app.use("/product", express.static("lib"));
+var product = require("./router/product");
+app.use("/product", product);
+app.get("/product", function (req, res) {
+  res.render("product");
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", product);
 
 // 結帳去(購買資訊輸入)
-app.use('/order', express.static('lib'));
+app.use("/order", express.static("lib"));
 app.get("/order", function (req, res) {
-    res.render('order');
-})
+  res.render("order");
+});
 
 // 確認訂單
-app.use('/orderconfirm', express.static('lib'));
+app.use("/orderconfirm", express.static("lib"));
 app.get("/orderconfirm", function (req, res) {
-    res.render('orderconfirm');
-})
+  res.render("orderconfirm");
+});
 
 // 送出訂單
-app.use('/orderprocessing', express.static('lib'));
+app.use("/orderprocessing", express.static("lib"));
 app.get("/orderprocessing", function (req, res) {
-    res.render('orderprocessing');
-})
+  res.render("orderprocessing");
+});
 
 // 完成訂單
-app.use('/orderplaced', express.static('lib'));
+app.use("/orderplaced", express.static("lib"));
 app.get("/orderplaced", function (req, res) {
-    res.render('orderplaced');
-})
+  res.render("orderplaced");
+});
 // 評價訂單
-app.use('/rateorder', express.static('lib'));
+app.use("/rateorder", express.static("lib"));
 app.get("/rateorder", function (req, res) {
-    res.render('rateorder');
-})
-
+  res.render("rateorder");
+});
 
 app.listen(2407, function () {
-    console.log("----伺服器啟動OK " + new Date().toLocaleTimeString() + "----");
-})
+  console.log("----伺服器啟動OK " + new Date().toLocaleTimeString() + "----");
+});
---
+++ router/product.js
@@ -0,0 +1,127 @@
+let express = require("express");
+let product = express.Router();
+let mysql = require("../DB");
+product.get("/", function (req, res) {
+  let sql = "SELECT product FROM product WHERE category = '蔬果箱';";
+  let sql1 =
+    "SELECT info.product, product_content.image FROM info JOIN product_content ON info.product = product_content.product WHERE info.season ORDER BY RAND() LIMIT 6;";
+  let sql2 =
+    "SELECT product.pid, product.product, product_content.content, product_content.feature, product_content.price, activity.off_act, activity.act_content,activity.act_content2,activity.act_content3,activity.act_time FROM product_content INNER JOIN product ON product_content.product = product.product LEFT JOIN activity ON product_content.product = activity.product WHERE product.category = '蔬果箱';"; // 新增的查询语句
+  let sql3 =
+    "SELECT product.product, product.category, product.firm, info.season FROM product INNER JOIN info ON product.product = info.product WHERE (product.category = '蔬菜' ) AND (info.season = '9,10,11' OR info.season = '全年' or info.season = '12,1,2');";
+  let sql4 =
+    "SELECT product.product, product.category, product.firm, info.season FROM product INNER JOIN info ON product.product = info.product WHERE (product.category = '水果' ) AND (info.season = '9,10,11' OR info.season = '全年' or info.season = '12,1,2'or info.season = '3,4,5');";
+  // 定义一个对象来存储三组数据
+
+  let data = {
+    names: [],
+    images: [],
+    con: [],
+    veg: [],
+    fruit: [],
+  };
+
+  mysql.query(sql, function (err, rows) {
+    if (err) {
+      console.log("水果箱名稱連結錯誤");
+      console.log(err);
+      res.status(500).send("数据库错误");
+      return;
+    }
+    // 将查询结果存储在data对象中
+    data.names = rows;
+
+    // 在第一个查询完成后，执行第二个查询
+    mysql.query(sql1, function (err, rows) {
+      if (err) {
+        console.log("圖片取得失敗");
+        console.log(err);
+        res.status(500).send("数据库错误");
+        return;
+      }
+      // 将第二个查询结果存储在data对象中
+      data.images = rows;
+
+      // 在第二个查询完成后，执行第三个查询
+      mysql.query(sql2, function (err, rows) {
+        if (err) {
+          console.log("產品資訊取得失敗");
+          console.log(err);
+          res.status(500).send("数据库错误");
+          return;
+        }
+        // 将第三个查询结果存储在data对象中
+        data.con = rows;
+
+        mysql.query(sql3, function (err, rows) {
+          if (err) {
+            console.log("蔬菜品項取得失敗");
+            console.log(err);
+            res.status(500).send("数据库错误");
+            return;
+          }
+          data.veg = rows;
+          mysql.query(sql4, function (err, rows) {
+            if (err) {
+              console.log("水果品項取得失敗");
+              console.log(err);
+              res.status(500).send("数据库错误");
+              return;
+            }
+
+            data.fruit = rows;
+
+            res.render("product", {
+              data: data,
+            });
+          });
+        });
+      });
+    });
+  });
+
+  product.use(express.json());
+  product.post("/cardData", (req, res) => {
+    const cardDataArr = req.body; // 获取请求中的JSON数据
+
+    // 循环遍历 cardDataArr 数组并插入数据库
+    cardDataArr.forEach((cardData) => {
+      const { productId, productName, commodity, sumValue } = cardData;
+
+      const sql =
+        "INSERT INTO text(bid, productName, commodity, sumValue) VALUES (?, ?, ?, ?)";
+      const values = [productId, productName, commodity, sumValue];
+
+      mysql.query(sql, values, function (err, results) {
+        if (err) {
+          console.error("插入数据时出错: " + err.message);
+          return;
+        }
+        console.log("成功插入数据，插入ID:", results.insertId);
+      });
+    });
+
+    res.send("数据插入成功"); // 发送响应
+  });
+
+  product.post("/delcardData", (req, res) => {
+    const productIdToDelete = req.body.productId; // 从请求中获取要删除的产品ID
+
+    // 使用 productIdToDelete 编写 SQL 查询语句
+    const deleteQuery = "DELETE FROM text WHERE bid = ?";
+
+    // 使用 MySQL 连接执行删除操作
+    mysql.query(deleteQuery, [productIdToDelete], (err, results) => {
+      if (err) {
+        console.error("删除数据时出错: " + err.message);
+        res.status(500).send("删除数据时出错");
+        return;
+      }
+
+      console.log("成功删除行数: " + results.affectedRows);
+      res.send("数据删除成功"); // 发送响应
+    });
+  });
+});
+
+module.exports = product;
