b4e52fbe5936213528d190b266e03b953cd102ce
cl940222 <cl940222@gmail.com>
2023-09-29 15:16:43 +0800

    更新後台用路徑

--- router/activityboard.js
+++ router/activityboard.js
@@ -20,6 +20,38 @@ activity.get('/', function(req, res){
     })
 });
 
+// 後臺抓所有活動資料
+activity.get('/all', function(req, res){
+    var sql = `SELECT actid, actTitle, actImg, date_format(acttime, '%Y-%c-%d') as acttime, actText, actCat from activityboard order by acttime desc;`;
+    db.query(sql, function(err, data){
+        if(err){
+            console.log('抓活動資料失敗');
+            console.log(err);
+        } else {
+            console.log('抓活動資料成功');
+            console.log(data);
+            res.send(data);
+        }
+    })
+})
+
+// 後臺抓單一活動資料
+activity.get('/:id', function(req, res){
+    var sql = `SELECT actid, actTitle, actImg, date_format(acttime, '%Y-%c-%d') as acttime, actText, actCat from activityboard where actid = ?;`;
+    var data = [req.params.id];
+    console.log(req.params.id);
+    db.query(sql, data, function(err, data){
+        if(err){
+            console.log('抓取單一活動失敗');
+            console.log(err);
+        } else {
+            console.log('抓取單一活動成功');
+            console.log(data);
+            res.send(data);
+        }
+    })
+})
+
 //文章內頁
 activity.get('/inside/:id', function(req, res){
     var act_id = req.params.id;
@@ -72,6 +104,60 @@ activity.post('/spanValue', jp, function(req, res){
             
         }
     })
+})
+
+// 後台新增活動
+activity.put('/new', jp, function(req, res){
+    console.log(req.body.aTitle);
+    var data = [req.body.aTitle, req.body.aImg, req.body.aContent, parseInt(req.body.aType)];
+    var sql =  `insert into activityboard (actTitle, actImg, actText, actCat) values (?, ?, ?, ?);`;
+    db.query(sql, data, function(err, row){
+        if(err){
+            console.log('新增活動失敗');
+            console.log(err);
+        } else {
+            console.log('新增活動成功');
+            console.log(res);
+            res.send(JSON.stringify(req.body));
+        }
+    })
+})
+
+// 後臺更新活動資料
+activity.put('/edit', jp, function(req, res){
+    const actid = parseInt(req.body.actid);
+    const actTitle = req.body.actTitle;
+    const actText = req.body.actText;
+    const actCat = parseInt(req.body.actCat);
+    const sql = `UPDATE activityboard SET actTitle=?, actText =?, actCat=? WHERE actid = ?;`
+    const data = [actTitle, actText, actCat, actid];
+    db.query(sql, data, function(err, rows){
+        if(err){
+            console.log('更新活動失敗')
+            console.log(err)
+        } else {
+            console.log('更新活動成功')
+            console.log(rows)
+            res.send(JSON.stringify(req.body));
+        }
+    })
+})
+
+// 後臺刪除活動資料
+activity.delete('/delete/:id', jp, function(req, res){
+    var data = [req.params.id];
+    console.log(data);
+    var sql = `delete from activityboard where actid = ?;`;
+    db.query(sql, data, function(err, scss){
+        if(err){
+            console.log('刪除活動失敗');
+            console.log(err);
+        } else {
+            console.log('刪除活動成功');
+            console.log(scss);
+            res.send(JSON.stringify(req.body));
+        }
+    }) 
 })
 
 
